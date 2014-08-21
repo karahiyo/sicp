f381041c4e12e8055891091af1f657ef19eea50d
Yusuke Shimizu <Yusuke_Shimizu@voyagegroup.com>
2014-08-21 21:29:37 +0900

    fix

--- ch.1/q5.scm
+++ ch.1/q5.scm
@@ -9,7 +9,7 @@
 
 ;; 処理系:gauche
 
-;; gaucheはnorma-order evaluationを採用している。
+;; gaucheはapplicative-order evaluationを採用している。
 ;; 今回の場合であれば、`(test 0 (p))`を実行した際には
 ;; `(p)`が無限に展開され続ける。
 
@@ -23,6 +23,6 @@
 ;;         At line 8 of "(stdin)"
 ;; --------
 
-;; applicative-order evaluationを採用しているlisp処理系ならば、
-;; 今回の式ではまず`(test 0 (p))`が評価され
+;; normal-order evaluationを採用している処理系ならば、
+;; 今回の式ではまず一番外側の式`(test 0 (p))`が評価され
 ;; 値0を返し正常終了する。
