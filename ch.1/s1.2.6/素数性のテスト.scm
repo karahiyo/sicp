a14945df281b6e4de3402301de1bcae23e6cc455
Yusuke Shimizu <Yusuke_Shimizu@voyagegroup.com>
2015-05-07 20:34:31 +0900

    :coffee:

---
+++ ch.1/s1.2.6/素数性のテスト.scm
@@ -0,0 +1,24 @@
+;; p.28
+;; 素数性のテスト
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square n)
+  (* n n))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(print (prime? 19))
+
+
+
