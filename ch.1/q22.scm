5822435f70e20ef3e51a8751e6a45ed93d3860a4
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2015-07-31 00:44:41 +0900

    ...

--- ch.1/q22.scm
+++ ch.1/q22.scm
@@ -30,13 +30,13 @@
   (display elapsed-time))
 
 ;; 指定範囲の連続する奇整数について素数性を調べる手続き
-(define (search-for-primes start-n count :optional (increment +))
+(define (search-for-primes start-n count)
   (cond ((= count 0) (display "\n--- finish ---"))
-        ((= (mod start-n 2) 0) (search-for-primes (increment start-n 1) count increment))
+        ((= (mod start-n 2) 0) (search-for-primes (+ start-n 1) count))
         (else (begin
                 (if (timed-prime-test start-n)
-                    (search-for-primes (increment start-n 2) (- count 1) increment)
-                    (search-for-primes (increment start-n 2) count increment))))))
+                    (search-for-primes (+ start-n 2) (- count 1))
+                    (search-for-primes (+ start-n 2) count))))))
 
 (search-for-primes 1000 3)
 ;; 1001
@@ -45,30 +45,18 @@
 ;; 1007
 ;; 1009 *** #<time-duration 0.000005000>
 ;; 1011
-;; 1013 *** #<time-duration 0.000003000>
+;; 1013 *** #<time-duration 0.000005000>
 ;; 1015
 ;; 1017
-;; 1019 *** #<time-duration 0.000003000>
+;; 1019 *** #<time-duration 0.000004000>
 ;; --- finish ---
 
-(search-for-primes 1000 3 -)
-;; 999
-;; 997 *** #<time-duration 0.000003000>
-;; 995
-;; 993
-;; 991 *** #<time-duration 0.000003000>
-;; 989
-;; 987
-;; 985
-;; 983 *** #<time-duration 0.000003000>
-;; --- finish ---
-
-(search-for-primes 10000 3 +)
+(search-for-primes 10000 3)
 ;; 10001
 ;; 10003
 ;; 10005
-;; 10007 *** #<time-duration 0.000010000>
-;; 10009 *** #<time-duration 0.000010000>
+;; 10007 *** #<time-duration 0.000011000>
+;; 10009 *** #<time-duration 0.000011000>
 ;; 10011
 ;; 10013
 ;; 10015
@@ -82,42 +70,12 @@
 ;; 10031
 ;; 10033
 ;; 10035
-;; 10037 *** #<time-duration 0.000015000>
+;; 10037 *** #<time-duration 0.000012000>
 ;; --- finish ---
 
-(search-for-primes 10000 3 -)
-;; 9999
-;; 9997
-;; 9995
-;; 9993
-;; 9991
-;; 9989
-;; 9987
-;; 9985
-;; 9983
-;; 9981
-;; 9979
-;; 9977
-;; 9975
-;; 9973 *** #<time-duration 0.000015000>
-;; 9971
-;; 9969
-;; 9967 *** #<time-duration 0.000014000>
-;; 9965
-;; 9963
-;; 9961
-;; 9959
-;; 9957
-;; 9955
-;; 9953
-;; 9951
-;; 9949 *** #<time-duration 0.000014000>
-;; --- finish ---
-
-
-(search-for-primes 100000 3 +)
+(search-for-primes 100000 3)
 ;; 100001
-;; 100003 *** #<time-duration 0.000050000>
+;; 100003 *** #<time-duration 0.000035000>
 ;; 100005
 ;; 100007
 ;; 100009
@@ -125,7 +83,7 @@
 ;; 100013
 ;; 100015
 ;; 100017
-;; 100019 *** #<time-duration 0.000032000>
+;; 100019 *** #<time-duration 0.000046000>
 ;; 100021
 ;; 100023
 ;; 100025
@@ -137,31 +95,12 @@
 ;; 100037
 ;; 100039
 ;; 100041
-;; 100043 *** #<time-duration 0.000043000>
+;; 100043 *** #<time-duration 0.000041000>
 ;; --- finish ---
 
-(search-for-primes 100000 3 -)
-;; 99999
-;; 99997
-;; 99995
-;; 99993
-;; 99991 *** #<time-duration 0.000053000>
-;; 99989 *** #<time-duration 0.000046000>
-;; 99987
-;; 99985
-;; 99983
-;; 99981
-;; 99979
-;; 99977
-;; 99975
-;; 99973
-;; 99971 *** #<time-duration 0.000046000>
-;; --- finish ---
-
-
-(search-for-primes 1000000 3 +)
+(search-for-primes 1000000 3)
 ;; 1000001
-;; 1000003 *** #<time-duration 0.000143000>
+;; 1000003 *** #<time-duration 0.000105000>
 ;; 1000005
 ;; 1000007
 ;; 1000009
@@ -176,39 +115,17 @@
 ;; 1000027
 ;; 1000029
 ;; 1000031
-;; 1000033 *** #<time-duration 0.000142000>
+;; 1000033 *** #<time-duration 0.000105000>
 ;; 1000035
-;; 1000037 *** #<time-duration 0.000185000>
+;; 1000037 *** #<time-duration 0.000105000>
 ;; --- finish ---
 
-(search-for-primes 1000000 3 -)
-;; 999999
-;; 999997
-;; 999995
-;; 999993
-;; 999991
-;; 999989
-;; 999987
-;; 999985
-;; 999983 *** #<time-duration 0.000128000>
-;; 999981
-;; 999979 *** #<time-duration 0.000114000>
-;; 999977
-;; 999975
-;; 999973
-;; 999971
-;; 999969
-;; 999967
-;; 999965
-;; 999963
-;; 999961 *** #<time-duration 0.000097000>
-;; --- finish ---
 
 ;; gosh> (log 10 2)
 ;; 3.3219280948873626
 
-;; 1000    =>  avg 0.000003
-;; 10000   =>  avg 0.000013 <- 約4.33倍. だいたい妥当
-;; 100000  =>  avg 0.000045 <- 約3.46倍. だいたい妥当
-;; 1000000 =>  avg 0.000134 <- 約2.97倍. だいたい妥当
+;; 1000    =>  avg 0.000004
+;; 10000   =>  avg 0.000011 <- 約2.75倍. だいたい妥当
+;; 100000  =>  avg 0.000040 <- 約3.63倍. だいたい妥当
+;; 1000000 =>  avg 0.000105 <- 約2.62倍. だいたい妥当
 
