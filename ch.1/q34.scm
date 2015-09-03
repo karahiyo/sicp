61b8f73939d8d2f87c42bab9680474eea6850070
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2015-09-04 00:08:53 +0900

    :bulb:

--- ch.1/q34.scm
+++ ch.1/q34.scm
@@ -5,10 +5,15 @@
 (define (f g)
   (g 2))
 
-(print (f square)) ;; => 4
+;(print (f square)) ;; => 4
 
-(print (f (lambda (z) (* z (+ z 1))))) ;; => 6
+;(print (f (lambda (z) (* z (+ z 1))))) ;; => 6
 
 (print (f f))
 ;; => gosh: "error": invalid application: (2 2)
+
+(f f)
+(f 2) ; <- f手続きが評価される。引数で渡されるf手続きではない
+(2 2) ; <- invalid application
+
 
