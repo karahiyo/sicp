71a8738ba90e22fe156ddb3b54aaec18467a7408
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2015-11-05 01:26:09 +0900

    ..

---
+++ ch.2/q10.scm
@@ -0,0 +1,24 @@
+;; Q 2.10
+
+(load "./ch.2/q7.scm")
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(print (div-interval (make-interval 1 2) (make-interval -1 -2)))
+(print (div-interval (make-interval 1 2) (make-interval -1 2)))
+; => (0.5 . 1.0)
+
+; こっちのが好み
+(define (div-interval x y)
+  (if (< (* (lower-bound y) (upper-bound y)) 0)
+    (error)
+    (mul-interval x
+                  (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y))))))
+
+(print (div-interval (make-interval 1 2) (make-interval -1 -2)))
+(print (div-interval (make-interval 1 2) (make-interval -1 2))) ; error
+
